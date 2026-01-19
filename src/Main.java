1bb121024f2ddb1e0ee123498bd3d1940310e311
Hugodev999 <hcoelhoteixeira2@gmail.com>
2026-01-19 15:33:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+@@ -1,12 +1,11 @@
+import br.com.dio.desafio.dominio.Bootcamp;
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
+import br.com.dio.desafio.dominio.Mentoria;
 package pacote.desafio;
 
 import pacote.desafio.dominio.*; // ESSENCIAL: Resolve o erro de "Cannot resolve symbol"
@@ -10,7 +15,6 @@ public class Main {
         curso1.setTitulo("curso java");
         curso1.setDescricao("descrição curso java");
         curso1.setCargaHoraria(8);
-
         Curso curso2 = new Curso();
         curso2.setTitulo("curso js");
         curso2.setDescricao("descrição curso js");
@@ -21,6 +25,10 @@ public class Main {
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
+
+        /*System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);*/
 
         // Criando o Bootcamp
         Bootcamp bootcamp = new Bootcamp();
@@ -35,31 +43,43 @@ public class Main {
         Dev devCamila = new Dev();
         devCamila.setNome("Camila");
         devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
         System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
-        
+
         devCamila.progredir();
         devCamila.progredir();
-        
+
         System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
         System.out.println("Conteúdos Inscritos Camila: " + devCamila.getConteudosInscritos());
         System.out.println("Conteúdos Concluídos Camila: " + devCamila.getConteudosConcluidos());
         System.out.println("XP Camila: " + devCamila.calcularTotalXp());
 
+        System.out.println("-------");
         System.out.println("\n------- Dev João -------");
 
         // Testando Dev João
         Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
         devJoao.setNome("João");
         devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
-        
+
         devJoao.progredir();
         devJoao.progredir();
         devJoao.progredir();
-        
+
         System.out.println("-");
+        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
+        System.out.println("XP:" + devJoao.calcularTotalXp());
+
         System.out.println("Conteúdos Inscritos João: " + devJoao.getConteudosInscritos());
         System.out.println("Conteúdos Concluídos João: " + devJoao.getConteudosConcluidos());
         System.out.println("XP João: " + devJoao.calcularTotalXp());
     }
+
 }
