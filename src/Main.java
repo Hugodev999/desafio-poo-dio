3158148aa8129f6bbd019aa7915c05823f1468f6
Hugodev999 <hcoelhoteixeira2@gmail.com>
2026-01-19 15:22:44 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,21 @@
 @@ -1,8 +1,10 @@
-import br.com.dio.desafio.dominio.Bootcamp;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Dev;
-import br.com.dio.desafio.dominio.Mentoria;
 package pacote.desafio;
 
 import pacote.desafio.dominio.*; // ESSENCIAL: Resolve o erro de "Cannot resolve symbol"
-import java.time.LocalDate;
 
-public class Main {
-    public static void main(String[] args) {
-        Curso curso1 = new Curso();
-        curso1.setTitulo("curso java");
-        curso1.setDescricao("descrição curso java");
-        curso1.setCargaHoraria(8);
 
-        Curso curso2 = new Curso();
-        curso2.setTitulo("curso js");
-        curso2.setDescricao("descrição curso js");
-        curso2.setCargaHoraria(4);
-
-        Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("mentoria de java");
-        mentoria.setDescricao("descrição mentoria java");
-        mentoria.setData(LocalDate.now());
-
-        /*System.out.println(curso1);
         *System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);*/
+      
 
         System.out.println(mentoria);*
-        Bootcamp bootcamp = new Bootcamp();
-        bootcamp.setNome("Bootcamp Java Developer");
-        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
-        bootcamp.getConteudos().add(curso1);
-        bootcamp.getConteudos().add(curso2);
-        bootcamp.getConteudos().add(mentoria);
-
+        
 @@ -36,10 +38,7 @@ public static void main(String[] args) {
-        Dev devCamila = new Dev();
-        devCamila.setNome("Camila");
-        devCamila.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        devCamila.progredir();
-        devCamila.progredir();
-        System.out.println("-");
+       
 @@ -43,9 +40,7 @@ public static void main(String[] args) {
-        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
-        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
-        System.out.println("XP:" + devCamila.calcularTotalXp());
+       
+@@ -48,16 +55,47 @@ public static void main(String[] args) {
 
-        System.out.println("-------");
-
-        Dev devJoao = new Dev();
-        devJoao.setNome("Joao");
-        devJoao.setNome("Carlos");
-        devJoao.inscreverBootcamp(bootcamp);
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        System.out.println("Conteúdos Inscritos Carlos:" + devJoao.getConteudosInscritos());
-        devJoao.progredir();
-        devJoao.progredir();
-        devJoao.progredir();
-        System.out.println("-");
-        System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos João:" + devJoao.getConteudosConcluidos());
-        System.out.println("Conteúdos Inscritos Carlos:" + devJoao.getConteudosInscritos());
-        System.out.println("Conteúdos Concluidos Carlos:" + devJoao.getConteudosConcluidos());
-        System.out.println("XP:" + devJoao.calcularTotalXp());
+      
 @@ -60,4 +59,30 @@ public static void main(String[] args) {
 
     }
